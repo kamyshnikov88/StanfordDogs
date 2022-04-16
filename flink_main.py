cb8d144a69be67a7c1e1d952c9a0230a78a4c2ad
kamyshnikov88 <68143449+kamyshnikov88@users.noreply.github.com>
2022-04-16 16:14:01 +0300

    second commit

--- flink_main.py
+++ flink_main.py
@@ -1,18 +1,17 @@
+import functools
 import json
-import sys
+
 from PIL import Image
 import requests
+from matplotlib import pyplot as plt
 from pyflink.common import Types
-from pyflink.datastream import StreamExecutionEnvironment, SourceFunction
+from pyflink.datastream import StreamExecutionEnvironment
 from pyflink.datastream.connectors import (FlinkKafkaConsumer,
                                            FlinkKafkaProducer)
 from pyflink.common.serialization import SimpleStringSchema
 from kafka_client import open_image
 from tfserving_prediction_getter import get_d
 from tfserving_prediction_getter import get_url
-import socket
-
-socket.setdefaulttimeout(100)
 
 
 def get_pred(img: Image) -> str:
@@ -31,11 +30,12 @@ def get_pred(img: Image) -> str:
     except requests.exceptions.HTTPError:
         raise Exception(response.json()['error'])
     pred = response.json()['predictions'][0]
+    print('get_pred FINALLY')
     return d[pred.index(max(pred))]
 
 
 def msg_to_pred(msg_list: list) -> list:
-    images = []
+    preds = []
     msg = b''
     prev_i = 0
     for msg in msg_list:
@@ -46,45 +46,84 @@ def msg_to_pred(msg_list: list) -> list:
                 msg += value
             else:
                 prev_i = int(key.split('|')[0])
-                images.append(get_pred(open_image(msg).resize((299, 299))))
+                preds.append(get_pred(open_image(msg).resize((299, 299))))
                 msg = value
         else:
             prev_i += 1
-            images.append(get_pred(open_image(msg).resize((299, 299))))
+            preds.append(get_pred(open_image(value).resize((299, 299))))
     if msg != b'':
-        images.append(get_pred(open_image(msg).resize((299, 299))))
-    return images
+        preds.append(get_pred(open_image(msg).resize((299, 299))))
+    return preds
+
+
+def decode_key(msg: str) -> tuple:
+    jsn = json.loads(msg)
+    i, j, value = jsn.values()
+    return i, j, value
+
+
+def fetch_parts(cum_msg, msg):
+    print(123)
+    cum_msg_v = cum_msg[-1]
+    if isinstance(cum_msg_v, str):
+        cum_msg_v = [tuple([cum_msg[1], cum_msg[-1]])]
+    cum_msg_v.append(tuple([msg[1], msg[-1]]))
+    return cum_msg[0], 0, cum_msg_v
+
+
+def sum_parts(msg: list):
+    # print(type(msg))
+    # print(type(msg))
+    # print(len(msg))
+    # msg = sorted(msg, key=lambda x: x[0])
+    # _, parts = zip(*msg)
+    # return sum(parts)
+    return 0
 
 
 def flink_main() -> None:
     env = StreamExecutionEnvironment.get_execution_environment()
-    # kafka_source = FlinkKafkaConsumer(
-    #     topics='img3',
-    #     deserialization_schema=SimpleStringSchema(),
-    #     properties={'bootstrap.servers': 'localhost:9092'})
-    # ds = env.add_source(kafka_source)
+    project_path = 'file:///Users/kamyshnikovy/PycharmProjects/StanfordDogs/'
+    flink_path = project_path + 'data/flink/'
+    jar_names = [
+        'flink-connector-kafka_2.12-1.14.4.jar',
+        'kafka-clients-2.4.1.jar',
+        'flink-connector-base-1.14.4.jar',
+        'flink-shaded-force-shading-14.0.jar',
+        'lz4-java-1.6.0.jar',
+        'slf4j-api-1.7.28.jar',
+        'snappy-java-1.1.7.3.jar',
+        'zstd-jni-1.4.3-1.jar'
+        ]
+    jar_names = list(map(lambda x: flink_path + x, jar_names))
 
-    ds = env.from_collection([b'123'], type_info=Types.BYTE())
-    msg_list = []
-    with ds.execute_and_collect() as results:
-        for msg in results:
-            msg_list.append(msg)
-    predictions = msg_to_pred(msg_list)
+    env.add_jars(*jar_names)
+    kafka_source = FlinkKafkaConsumer(
+        topics='img',
+        deserialization_schema=SimpleStringSchema(),
+        properties={'bootstrap.servers': 'localhost:9092',
+                    'auto.offset.reset': 'earliest'
+                    })
+    ds = env.add_source(kafka_source)
 
-    ds = env.from_collection(
-        collection=predictions,
-        type_info=Types.STRING())
+    ds = (ds
+          .map(decode_key)
+          .key_by(lambda x: x[0])
+          .reduce(lambda c, d: fetch_parts(c, d))
+          .map(lambda z: print(len(z[-1])))
+          .map(sum_parts)
+          .map(lambda x: get_pred(open_image(x.encode('latin1')).resize((299, 299)))))
 
-    kafka_sink = FlinkKafkaProducer(
-        topic='predictions',
-        serialization_schema=SimpleStringSchema(),
-        producer_config={'bootstrap.servers': 'localhost:9092'})
-    ds.add_sink(kafka_sink)
+    # kafka_sink = FlinkKafkaProducer(
+    #     topic='predictions',
+    #     serialization_schema=SimpleStringSchema(),
+    #     producer_config={'bootstrap.servers': 'localhost:9092'})
+    # ds.add_sink(kafka_sink)
     env.execute('flink_main')
 
 
 if __name__ == '__main__':
-    flink_main()
+    # flink_main()
 
 
 
--- kafka_client.py
+++ kafka_client.py
@@ -1,9 +1,16 @@
+from matplotlib import pyplot as plt
+
 from trainer import get_images_paths
 from kafka import KafkaProducer, KafkaConsumer
 from PIL import Image
+import json
+from bson import json_util
 
-IMAGE_PATH = './data/Images/n02092002-Scottish_deerhound/n02092002_6621.jpg'
 SEP = b'S19283746E'
+
+
+def get_separator():
+    return SEP
 
 
 def int_to_bytes(x: int) -> bytes:
@@ -23,8 +30,8 @@ def get_extended_byte_string(img: Image):
     """
     w, h = img.size
     wb = int_to_bytes(w)
-    wh = int_to_bytes(h)
-    b_img_size = wb + SEP + wh + SEP
+    hb = int_to_bytes(h)
+    b_img_size = wb + SEP + hb + SEP
     b_size_with_image = b_img_size + img.tobytes()
     return b_size_with_image
 
@@ -34,28 +41,32 @@ def open_image(string: bytes) -> Image:
     img = Image.frombytes('RGB',
                           (int_from_bytes(w), int_from_bytes(h)),
                           img)
+    img.show()
+    plt.show()
     return img
 
 
 def images_to_kafka() -> None:
-    img_paths = get_images_paths()[:1]
+    img_paths = get_images_paths()[:5]
     producer = KafkaProducer(bootstrap_servers='localhost:9092')
     count = 0
     for i, img_path in enumerate(img_paths):
         img = Image.open(img_path)
-        bs = get_extended_byte_string(img)
+        bs = get_extended_byte_string(img).decode('latin1')
         lbs = len(bs)
-        f = 400000
+        f = 150000
         if lbs > f:  # if kafka message can't contain whole image
-            for j in range(lbs // f + 1):
+            for j in range(lbs//f + 1):
                 count += 1
                 sub_msg = bs[j*f:(j+1)*f]
-                key = str.encode(f'{i}|{j}')
-                producer.send('img', key=key, value=sub_msg)
+                value = {'i': i, 'j': j, 'json_value': sub_msg}
+                value = json.dumps(value).encode('latin1')
+                producer.send('img', value=value)
         else:
             count += 1
-            key = str.encode(f'{i}')
-            producer.send('img', key=key, value=bs)
+            value = {'i': i, 'j': 0, 'json_value': bs}
+            value = json.dumps(value).encode('latin1')
+            producer.send('img', value=value)
     print('total message count:', count)
 
 
@@ -65,23 +76,21 @@ def read_topic(topic_name: str) -> None:
                              auto_offset_reset='earliest',
                              consumer_timeout_ms=1000)
     images = []
-    msg = b''
+    msg = ''
     prev_i = 0
     for item in consumer:
-        key = item.key.decode()
-        value = item.value
-        if '|' in key:
-            if int(key.split('|')[0]) == prev_i:
-                msg += value
-            else:
-                prev_i = int(key.split('|')[0])
-                images.append(open_image(msg))
-                msg = value
+        d = item.value.decode('latin1')
+        jsn = json.loads(d)
+        i, j, value = jsn.values()
+
+        if i == prev_i:
+            msg += value
         else:
-            prev_i += 1
-            images.append(open_image(value))
+            prev_i = i
+            images.append(open_image(msg.encode('latin1')))
+            msg = value
     if msg != b'':
-        images.append(open_image(msg))
+        images.append(open_image(msg.encode('latin1')))
     print(len(images))
 
 
